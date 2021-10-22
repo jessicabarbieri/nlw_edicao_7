1827f221e198e48446222932728785e0bc518896
jessica.barbieri <jessicabarbieri10@gmail.com>
2021-10-22 18:55:44 -0300

    cadastrar a mensagem

--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -15,74 +15,74 @@ interface IUserResponse {
 
 class AuthenticateUserService {
     async execute(code: string) {
-    const url = "https://github.com/login/oauth/access_token";
-    
-    /**
-     * Recuperando access_token
-     */
-    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
-        params: {
-            client_id: process.env.GITHUB_CLIENT_ID,
-            client_secret: process.env.GITHUB_CLIENT_SECRET,
-            code,
-        }, 
-        headers: {
-            "Accept": "application/json"
-        }
-    })
+        const url = "https://github.com/login/oauth/access_token";
 
-    /**
-     * Recuperando infos do user
-     */
-    const response = await axios.get<IUserResponse>(
-        "https://api.github.com/user", 
-        { 
-            headers: {
-                authorization: `Bearer ${accessTokenResponse.access_token}`,
+        /**
+         * Recuperando access_token
+         */
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
             },
-        }
-    );
-    
-    /**
-     * Verificando se o user existe no DB
-     * SIM -> Gera token
-     * NÃO -> Cria no DB e depois gera o token
-     */ 
-    const { login, id, avatar_url, name } = response.data
-
-    let user = await prismaClient.user.findFirst({ 
-        where: { 
-            github_id: id
-        }
-    })
-
-    if(!user) {
-        user = await prismaClient.user.create({
-            data: {
-                github_id: id,
-                login,
-                avatar_url,
-                name
+            headers: {
+                "Accept": "application/json"
             }
         })
-    }
 
-    const token = sign(
-        {
-            user: {
-                name: user.name,
-                avatar_url: user.avatar_url,
-                id: user.id
+        /**
+         * Recuperando infos do user
+         */
+        const response = await axios.get<IUserResponse>(
+            "https://api.github.com/user",
+            {
+                headers: {
+                    authorization: `Bearer ${accessTokenResponse.access_token}`,
+                },
             }
-        },
-        process.env.JWT_SECRET,
-        {
-            subject: user.id,
-            expiresIn: "1d"
-        }
-    );
+        );
 
-    return { token, user };
+        /**
+         * Verificando se o user existe no DB
+         * NÃO -> Cria no DB e depois gera o token
+         * SIM -> Gera token
+         */
+        const { login, id, avatar_url, name } = response.data
+
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+
+        if (!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+
+        const token = sign(
+            {
+                user: {
+                    name: user.name,
+                    avatar_url: user.avatar_url,
+                    id: user.id
+                }
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        );
+
+        return { token, user };
 
     }
 }
